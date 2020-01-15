77c03cccddd9ecb8b0d4a515271ce3562284a2ae
Hannes Rüger <hannesrueger@gmx.de>
2020-01-14 18:49:53 -1000

    added headline, cleaned up

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,14 @@
-console.log(process.argv);
+const figlet = require('figlet');
+const chalk = require('chalk');
+const clear = require('clear');
+
+export async function cli(argv) {
+    clear();
+    console.log(chalk.cyan("Welcome to"));
+    console.log(chalk.cyan(figlet.textSync('Containerizer')));
+    console.log(chalk.cyan("v1.0.0"));
+    console.log("\n");
+    console.log(chalk.yellow("Thanks for using this tool. Feel free to contribute by creating pull requests or reporting issues on GitHub!"));
+    
+    // console.log(argv);
+}
