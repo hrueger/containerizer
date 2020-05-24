fbfe5a7df841520d9670464dc29c3f090e85a4be
Hannes Rüger <hannesrueger@gmx.de>
2020-05-24 11:01:02 +0200

    fast update mode also active when changing env

--- laterInContainer/install.js
+++ laterInContainer/install.js
@@ -1,104 +1,103 @@
 const fs = require("fs");
 const path = require("path");
 const rimraf = require("rimraf");
-const {exec} = require("child_process");
+const { exec } = require("child_process");
 
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, "containerizer.json")));
 
-exports.install = async (updating=false) => {
+exports.install = async(updating = false) => {
 
-    const taskz = require("taskz");
- 
-    const tasks = taskz([
-        {
-            text: "Cleaning up old artifacs",
-            task: async () => {
-                setStatus(1, 9, "Cleaning up");
-                if (!updating || !config.fastUpdateMode) {
-                    await new Promise((resolve, reject) => {
-                        if (fs.existsSync(relativeToAbsolute(config.workingDirPath))) {
-                            rimraf(relativeToAbsolute(config.workingDirPath), (err) => {
-                                if (err) {
-                                    reject(err);
-                                } else {
-                                    resolve(err);
+        const taskz = require("taskz");
+
+        const tasks = taskz([{
+                        text: "Cleaning up old artifacs",
+                        task: async() => {
+                            setStatus(1, 9, "Cleaning up");
+                            if (!config.fastUpdateMode) {
+                                await new Promise((resolve, reject) => {
+                                    if (fs.existsSync(relativeToAbsolute(config.workingDirPath))) {
+                                        rimraf(relativeToAbsolute(config.workingDirPath), (err) => {
+                                            if (err) {
+                                                reject(err);
+                                            } else {
+                                                resolve(err);
+                                            }
+                                        });
+                                    } else {
+                                        resolve();
+                                    }
+                                });
+                            }
+                        }
+                    },
+                    {
+                        text: "Creating workspace",
+                        task: async() => {
+                            setStatus(2, 9, "Creating workspace");
+                            if (!config.fastUpdateMode) {
+                                fs.mkdirSync(relativeToAbsolute(config.workingDirPath));
+                            }
+                        }
+                    },
+                    {
+                        text: `Cloning ${config.repository} using branch ${config.branch}`,
+                        task: async() => {
+                            setStatus(3, 9, "Downloading");
+                            const simpleGit = require("simple-git/promise")(relativeToAbsolute(config.workingDirPath));
+                            if (!config.fastUpdateMode) {
+                                await simpleGit.clone(config.repository, ".", [`-b${config.branch}`]);
+                            } else {
+                                await simpleGit.pull();
+                            }
+                        }
+                    },
+                    {
+                        text: config.commit ? `Checking out commit ${config.commit}` : `Checking out latest commit`,
+                        task: async() => {
+                            setStatus(4, 9, "Downloading");
+                            if (!config.fastUpdateMode) {
+                                const simpleGit = require("simple-git/promise")(relativeToAbsolute(config.workingDirPath));
+                                await simpleGit.checkout(config.commit ? config.commit : config.branch);
+                            }
+                        }
+                    },
+                    {
+                        text: "Running \"npm install\"",
+                        tasks: taskz(config.npmInstallDirs.map((dir, index) => {
+                            return {
+                                text: `in /${dir}`,
+                                task: async() => {
+                                    setStatus(5, 9, `Installing dependencies ${index + 1} of ${config.npmInstallDirs.length}`);
+                                    await execShellCommand("npm install", { cwd: insideWorkDirPath(dir) });
                                 }
+                            };
+                        })),
+                    },
+                    {
+                        text: "Creating secret files",
+                        tasks: taskz(config.filesToCreate.map((file) => {
+                            setStatus(6, 9, "Creating config");
+                            const data = {};
+                            Object.keys(file.presetProperties).map((key) => {
+                                data[key] = file.presetProperties[key];
                             });
-                        } else {
-                            resolve();
-                        }
-                    });
-                }
-            }
-        },
-        {
-            text: "Creating workspace",
-            task: async () => {
-                setStatus(2, 9, "Creating workspace");
-                if (!updating || !config.fastUpdateMode) {
-                    fs.mkdirSync(relativeToAbsolute(config.workingDirPath));
-                }
-            }
-        },
-        {
-            text: `Cloning ${config.repository} using branch ${config.branch}`,
-            task: async () => {
-                setStatus(3, 9, "Downloading");
-                const simpleGit = require("simple-git/promise")(relativeToAbsolute(config.workingDirPath));
-                if (!updating || !config.fastUpdateMode) {
-                    await simpleGit.clone(config.repository, ".", [`-b${config.branch}`]);
-                } else {
-                    await simpleGit.pull();
-                }
-            }
-        },
-        {
-            text: config.commit ? `Checking out commit ${config.commit}` : `Checking out latest commit`,
-            task: async () => {
-                setStatus(4, 9, "Downloading");
-                if (!updating || !config.fastUpdateMode) {
-                    const simpleGit = require("simple-git/promise")(relativeToAbsolute(config.workingDirPath));
-                    await simpleGit.checkout(config.commit ? config.commit : config.branch);
-                }
-            }
-        },
-        { 
-            text: "Running \"npm install\"",
-            tasks: taskz(config.npmInstallDirs.map((dir, index) => {
-                    return {
-                        text: `in /${dir}`,
-                        task: async () => {
-                            setStatus(5, 9, `Installing dependencies ${index + 1} of ${config.npmInstallDirs.length}`);
-                            await execShellCommand("npm install", {cwd: insideWorkDirPath(dir)});
-                        }
-                    };
-                })),
-        },
-        {
-            text: "Creating secret files",
-            tasks: taskz(config.filesToCreate.map((file) => {
-                setStatus(6, 9, "Creating config");
-                const data = {};
-                Object.keys(file.presetProperties).map((key) => {
-                    data[key] = file.presetProperties[key];
-                });
-                file.properties.map((key) => {
-                    data[key] = process.env[key];
-                });
-                return {
-                    text: `file /${file.path} with template ${file.template}`,
-                    task: () => {
-                        if (file.template == "typescript") {
-                            fs.writeFileSync(insideWorkDirPath(file.path), `export const ${file.rootVariableName} = ${JSON.stringify(data)};`);
-                        } else {
-                            throw new Error(`Template ${file.template} is not supported!`);
-                        }
-                    }
-                };
-            })),
-        },
-        {
-            text: `Building Angular app from ${config.ngSrcDir} with ${config.customNgBuildCmd ? "custom" : "standard"} build command ${config.customNgBuildCmd ? `(${config.customNgBuildCmd})` : ""}`,
+                            file.properties.map((key) => {
+                                data[key] = process.env[key];
+                            });
+                            return {
+                                text: `file /${file.path} with template ${file.template}`,
+                                task: () => {
+                                    if (file.template == "typescript") {
+                                        fs.writeFileSync(insideWorkDirPath(file.path), `export const ${file.rootVariableName} = ${JSON.stringify(data)};`);
+                                    } else {
+                                        throw new Error(`Template ${file.template} is not supported!`);
+                                    }
+                                }
+                            };
+                        })),
+                    },
+                    {
+                        text: `Building Angular app from ${config.ngSrcDir} with ${config.customNgBuildCmd ? "custom" : "standard"} build command ${config.customNgBuildCmd ? `(${config.customNgBuildCmd})` : ""}`,
             task: async () => {
                 setStatus(7, 9, `Installing 1 of ${config.additionalBuilds.length + 1}`);
                 let buildCmd = (config.customNgBuildCmd ? config.customNgBuildCmd : `node --max-old-space-size=8192 ./node_modules/@angular/cli/bin/ng build --prod --build-optimizer=false --baseHref ${process.env.baseHref ? process.env.baseHref : "/"} --outputPath ${insideWorkDirPath(config.ngDestDir)}`);
